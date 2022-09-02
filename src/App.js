ec59a0f4eb237c4a3e3405ec7dbd21444ed3523c
firalammii <firalammii@gmail.com>
2022-09-02 13:00:57 +0300

    check

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Button } from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom'
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello THERE !!</h1>
+      <h2>WELCOME to Dispute Management System</h2>
+      <div>
+        <Button onClick = {()=>navigate('/customer/signup')}> SIGNUP </Button>{"  "}
+    
+        <Button onClick = {()=>navigate('/customer/login')}> LOGIN </Button>
+      </div>
     </div>
   );
 }
